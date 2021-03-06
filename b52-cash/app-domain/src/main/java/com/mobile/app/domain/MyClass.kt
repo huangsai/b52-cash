ee783f24e053aaef671a7d16924029308af39be9
barry <barryme2049@gmail.com>
2021-03-06 22:55:24 +0800

    升级依赖版本

---
+++ b52-cash/app-domain/src/main/java/com/mobile/app/domain/MyClass.kt
@@ -0,0 +1,7 @@
+package com.mobile.app.domain
+
+import android.app.Application
+
+class MyClass {
+    val ss = Application()
+}
---
+++ b52-cash/app-domain/src/test/java/com/mobile/app/domain/JvmTest.kt
@@ -0,0 +1,23 @@
+package com.mobile.app.domain
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+@RunWith(MockitoJUnitRunner::class)
+class JvmTest {
+
+    @BeforeTest
+    fun beforeTest() {
+    }
+
+    @Test
+    fun test_login() {
+    }
+
+    @AfterTest
+    fun afterTest() {
+    }
+}
